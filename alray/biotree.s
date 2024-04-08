da6470e17c01bb6d23af30d875460ca22b636a8a
alrayqiu <1780284652@qq.com>
2024-04-08 20:07:21 +0000

    im noob

--- alray/biotree.s
+++ alray/biotree.s
@@ -6,32 +6,62 @@ main:
     la      $t5,    output
 
 input:  
-    addi    $v0,    $0,         5       # system call #6 - input float
-    syscall                             # execute1
+    addi    $v0,    $0,         5           # system call #6 - input float
+    syscall                                 # execute1
     bltz    $v0,    endinput
     sw      $v0,    0($t0)
-    addi    $t0,    $t0,        4       # $t0 = $t0 + 4
+    addi    $t0,    $t0,        4           # $t0 = $t0 + 4
     j       input
 
 endinput:
 
+    la      $t2,    tree
+    li      $t1,    1
+    lw      $t4,    0($t2)
+    beq     $t2,    $t0,        endprintree
+    addi    $v0,    $0,         1           # system call #1 - print int
+    add     $a0,    $0,         $t4
+    syscall                                 # execute
+printtree:
+    li      $t3,    0
+    addi    $v0,    $0,         4           # system call #4 - print string
+    la      $a0,    endl
+    syscall                                 # execute
+    add     $t1,    $t1,        $t1
+ptloop: 
+    addi    $t2,    $t2,        4
+    beq     $t2,    $t0,        endprintree
+    lw      $t4,    0($t2)
+    addi    $v0,    $0,         1           # system call #1 - print int
+    add     $a0,    $0,         $t4
+    syscall                                 # execute
+    addi    $t3,    $t3,        1
+    beq     $t3,    $t1,        printtree
+    j       ptloop
+
+endprintree:
+    addi    $v0,    $0,         4           # system call #4 - print string
+    la      $a0,    endl
+    syscall                                 # execute
+
+
     la      $t0,    tree
 
-    move    $t1,    $t0                 # $t1 = $t2
+    move    $t1,    $t0                     # $t1 = $t2
     jal     search
     la      $t0,    output
 
 print:  
     lw      $t1,    0($t0)
-    addi    $v0,    $0,         1       # system call #1 - print int
+    addi    $v0,    $0,         1           # system call #1 - print int
     add     $a0,    $0,         $t1
-    syscall                             # execute
+    syscall                                 # execute
 
-    addi    $v0,    $0,         4       # system call #4 - print string
+    addi    $v0,    $0,         4           # system call #4 - print string
     la      $a0,    split
-    syscall                             # execute
+    syscall                                 # execute
     addi    $t0,    $t0,        4
-    ble     $t0,    $t5,        print   # if $t0 != $t5 then goto print
+    blt     $t0,    $t5,        print       # if $t0 != $t5 then goto print
 
 endprint:
 
@@ -40,26 +70,27 @@ endprint:
     syscall 
 
 search: 
-    sub     $t2,    $t1,        $t0     # $t1 = $t0 - $t2
-    addi    $t2,    $t2,        4       # $t2 = $t2 + 1
+    sub     $t2,    $t1,        $t0         # $t1 = $t0 - $t2
+    addi    $t2,    $t2,        4           # $t2 = $t2 + 1
 
-    add     $t3,    $t2,        $t2     # $t2, *, $t1, =, Hi, and, Lo, registers
-    subi    $t2,    $t3,        4       # $t3 = $t3 - 4
+    add     $t2,    $t2,        $t2         # $t2, *, $t1, =, Hi, and, Lo, registers
+    subi    $t2,    $t2,        4           # $t3 = $t3 - 4
 
     add     $t2,    $t2,        $t0
-    add     $t3,    $t3,        $t0
 
 
     lw      $t4,    0($t2)
     beqz    $t4,    re
-    subi    $sp,    $sp,        8
+    subi    $sp,    $sp,        12
     sw      $t1,    0($sp)
     sw      $ra,    4($sp)
+    sw      $t2,    8($sp)
     move    $t1,    $t2
-    jal     search                      # jump to search and save position to $ra
+    jal     search                          # jump to search and save position to $ra
     lw      $t1,    0($sp)
     lw      $ra,    4($sp)
-    addi    $sp,    $sp,        8       # $sp = $sp + 4
+    lw      $t2,    8($sp)
+    addi    $sp,    $sp,        12          # $sp = $sp + 4
 
 re:     
     lw      $t4,    0($t1)
@@ -71,14 +102,14 @@ re:
     subi    $sp,    $sp,        8
     sw      $t1,    0($sp)
     sw      $ra,    4($sp)
-    addi    $t1,    $3,         4
-    jal     search                      # jump to search and save position to $ra
+    addi    $t1,    $t2,        4
+    jal     search                          # jump to search and save position to $ra
     lw      $t1,    0($sp)
     lw      $ra,    4($sp)
-    addi    $sp,    $sp,        8       # $sp = $sp + 4
+    addi    $sp,    $sp,        8           # $sp = $sp + 4
 
 ret:    
-    jr      $ra                         # jump to $ra
+    jr      $ra                             # jump to $ra
 
 endsearch:
 
@@ -89,3 +120,5 @@ tree:   .space  256
 output: .space  256
 
 split:  .asciiz "\\/"
+leave:  .asciiz "|\\"
+endl:   .asciiz "\n"

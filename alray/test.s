ed6698a016cbb1c538a4527d26b3e609521be8a1
alrayqiu <1780284652@qq.com>
2024-04-17 02:38:33 +0000

    dayly

--- alray/test.s
+++ alray/test.s
@@ -3,26 +3,31 @@
     ##  programed by：stevie zou
     ##
 
+.data   
+save:   .asciiz "save\n"
+
+    ##
+    ############end of file
+
     #########################################
 
     #  text segment       #
 
     #########################################
 
-.text
-    .globlmain
-main:
-
-
+.text   
+main:   
+    li    $v0,      4   # system call #4 - print string
+    la      $a0,    save
+    syscall                     # execute
+    li $t1,0
+    
+    li      $a0, 0          # set exit code
+    li      $v0, 10         # 10 is the syscall to exit
+    syscall
+    
     #########################################
 
     #   data segment      #
 
     #########################################
-
-.data
-
-
-
-    ##
-    ############end of file

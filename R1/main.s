11e905693f89d894033871321c6a3182c34ee1f7
alrayqiu <1780284652@qq.com>
2024-05-08 02:43:06 +0000

    r1 compelete

--- R1/main.s
+++ R1/main.s
@@ -13,6 +13,7 @@ error2:     .asciiz "out of range,retry\n"
 buffer:     .space  20
 
 .text   
+    #entry point
 main:                                                               # entry point
     jal     create_primes                                           #call CreatePrime
     move    $t0,                $v0                                 #save return val to t0
@@ -25,13 +26,13 @@ main:                                                               # entry poin
     addi    $v0,                $0,                 4               # system call #4 - print string
     la      $a0,                endl
     syscall                                                         # execute
-    sll     $t1,                $t0,                1               # $t1 = $t0 >> 1
+    sll     $t1,                $t0,                1               # $t1 = $t0 << 1
     la      $t2,                primes
     add     $t1,                $t2,                $t1             # t1 = end of primes
     li      $t2,                9                                   # t2 = 9(while t2 > 0)'
     la      $v0,                max_prime
-    sh      $v0,                -2($t1)                             #max_prime = *t1
-    addi    $s0,                $t1,                -2              # s0 = t1 - 2
+    sh      $v0,                2($t1)                              #max_prime = *t1
+    move    $s0,                $t1
     addi    $t1,                $t1,                -20             # t1 = t1 - 10
     addi    $v0,                $0,                 4               # system call #4 - print string
     la      $a0,                msg1
@@ -47,12 +48,13 @@ print_last_ten:                                                     # print last
     la      $a0,                split
     syscall                                                         # execute
     addi    $t2,                $t2,                -1              # $t2 = $t2 - 1
-    bnez    $t2,                print_last_ten                      #while(t2 > 0)
+    bgez    $t2,                print_last_ten                      #while(t2 > 0)
 
     addi    $v0,                $0,                 4               # system call #4 - print string
     la      $a0,                endl
     syscall                                                         # execute
 call_for_input:                                                     # loop and get n while n >= 1
+    addi    $t1,                $t1,                -2              #t1 = t1 - 1
     jal     scanf_uint32
     andi    $v1,                $v0,                0xfffffffe      # $v0 = $v0 | 0x1
     beqz    $v1,                exit                                # n <= 1
@@ -68,6 +70,12 @@ exit:                                                               # quit
     li      $v0,                10                                  # 10 is the syscall to exit
     syscall 
 
+    # func Check_Prime(v0:R,a0:N,a1:T)
+    # N: num to check
+    # T: Top of primes set
+    # R: result 0 is a prime,1 not a prim
+    #       
+    # check if N is a prime
 check_prime:                                                        #v0:C CheckPrime(a0:N,a1:S) N is num to check,T is top point of primes
     addi    $sp,                $sp,                -12             # alloc 2 world in stack
     sw      $t0,                0($sp)                              #save t0
@@ -75,7 +83,7 @@ check_prime:                                                        #v0:C CheckP
     sw      $t2,                8($sp)                              #save t1
     move    $t0,                $a0
     li      $t1,                2
-    blt     $t1,                $t0,                not_l           #if n < 2
+    bltu    $t1,                $t0,                not_l           #if n < 2
     li      $v0,                0
     j       endcheck_prime
 not_l:                                                              #if n == 2
@@ -84,21 +92,22 @@ not_l:                                                              #if n == 2
     j       endcheck_prime
 not_two:                                                            #if n != 2 && n is odd
     andi    $t1,                $t0,                1               # $t0 = $t0 & 1
-    bnez    $t1,                not
+    bnez    $t1,                not_
     li      $v0,                0
     j       endcheck_prime
-not:        
+not_:       
     la      $t1,                primes
-    srl     $t2,                $t0,                1               # $t0 = $t0 >> 1
     li      $v0,                1
 chploop:    
     lhu     $t3,                0($t1)
-    blt     $t2,                $t3,                endcheck_prime
-    div     $t0,                $t3
+    multu   $t3,                $t3
+    mflo    $t2
+    bltu    $t0,                $t2,                endcheck_prime
+    divu    $t0,                $t3
     mfhi    $t3                                                     # $t3 = $t0 % $t3
     beqz    $t3,                endchploop                          #if(t3) then is not prime
-    addi    $t1,                $t1,                2               #else continue
-    ble     $a1,                $t1,                endcheck_prime
+    addiu   $t1,                $t1,                2               #else continue
+    bleu    $a1,                $t1,                endcheck_prime
     j       chploop
 endchploop: 
     li      $v0,                0
@@ -110,6 +119,9 @@ endcheck_prime:
     jr      $ra
     #end fun CheckPrime v0 == 0 is not prime=====================
 
+    # func CreatePrime(v0:PN)
+    # PN: size of primes set
+    # init the primes set ,primes < 0x10000
 create_primes:                                                      #v0:PN CreatePrimes() PN is size of primes
     addi    $sp,                $sp,                -4              # $sp = $sp - 4
     sw      $ra,                0($sp)                              # push ra to stack
@@ -142,7 +154,7 @@ cpnotprime:                                                         # else conti
 notwatchdog:
     #===
 
-    addi    $t0,                $t0,                2
+    addiu   $t0,                $t0,                2
     blt     $t2,                $t0,                endcploop
     j       cploop
 endcploop:  
@@ -155,11 +167,17 @@ endcreate_primes:
     jr      $ra
     # end create_prime ================================================
 
+    # func NextPrime(v0:PN,a0:N,a2:T
+    # N: num input
+    # T: top of primes list
+    # PN: next prime
+    #       
+    # search for next prime
 next_prime:                                                         #v0:PN next_prime(a0:N,a2:S) n is the input val
     addi    $sp,                $sp,                -4              # $sp = $sp - 4
     sw      $ra,                0($sp)                              # push ra to stack
-    la      $t1,                max_prime
-    blt     $a0,                $t1,                search_in       # if $a0 < $t1 then goto search_in
+    lb      $t1,                0($a2)
+    bltu    $a0,                $t1,                search_in       # if $a0 < $t1 then goto search_in
     move    $a1,                $a2                                 # $a1 = $a2
     jal     increase_search                                         # call increase_search(a0:n,a1:size)
     j       endnext_prime
@@ -173,6 +191,13 @@ endnext_prime:
     jr      $ra
     # end search ====================================================
 
+    # func BioSearch(a0:N,a1:l,a2:r,v0:PN)
+    # N: num input
+    # l: left point
+    # r: right point
+    # PN: next prime
+    #       
+    # search next primes when n < primes_set[T],use bio_search
 bio_search:                                                         # v0:PN bio_search(a0:N,a1:l,a2:r) l is pointer of left,r is pointer of right
     addi    $sp,                $sp,                -4              #alloc and push
     sw      $ra,                0($sp)
@@ -183,7 +208,7 @@ bioloop:
     srl     $t0,                $t0,                1
     andi    $t0,                $t0,                0xfffffffe
     lhu     $t2,                0($t0)
-    blt     $t2,                $a0,                bioless         # if $t2 < $a0 then goto bioless
+    bltu    $t2,                $a0,                bioless         # if $t2 < $a0 then goto bioless
     addi    $a2,                $t0,                -2
     j       bioloop
 bioless:    
@@ -196,26 +221,36 @@ endbio_search:
     jr      $ra
     #end bio_search ========================================
 
+    # func IncreaseSearch(a0:N,a1:T,v0:PN)
+    # N: num input
+    # T: top of primes size
+    # PN: next prime
+    #       
+    # search next primes when n > primes_set[T]
 increase_search:                                                    # v0:N v1:C increase_search(a0:N,a1:size)
     move    $v0,                $a1                                 #$v0 = $a1
     addi    $sp,                $sp,                -4              # $sp = $sp - 4
     sw      $ra,                0($sp)                              # push ra to stack
-    move    $t0,                $a0                                 # $t0 = $a0
     li      $v1,                0                                   # $v1 = 1
-
+    addiu   $t0,                $a0,                -1              # t0 = a0 -1
 isloop:     
-    bltu    $t0,                $a0,                outofrange      # if $t0 < $a0 then goto outofrange
+    addiu   $t0,                $t0,                1
+    bltu    $t0,                $a0,                outofrange      # if $t0 < $a0 then goto
+    move    $a0,                $t0                                 # pass the param N
     jal     check_prime                                             # call check_prime
     bnez    $v0,                endincrease_search
-    addiu   $t0,                $t0,                1
+    j       isloop
 outofrange: 
     li      $v1,                1
 endincrease_search:
+    move    $v0,                $t0
     lw      $ra,                0($sp)
     addi    $sp,                $sp,                4               # pop and free
     jr      $ra
     # end increase_search ===========================================
 
+    # func printuint32
+    # output uint32
 print_uint32:                                                       # print uint32
     bnez    $a1,                printofr
     move    $t0,                $a0                                 # $t0 = $a0
@@ -249,6 +284,8 @@ endprint_uint32:
     jr      $ra
     # end printuint32 ==================================================
 
+    # func ScanfUint32
+    # input uint32
 scanf_uint32:
     li      $t2,                0x0A
     li      $t3,                9
@@ -259,7 +296,7 @@ scanf_uint32:
     move    $t0,                $a0                                 # $t0 = $v0
     li      $v0,                0
     li      $v1,                0
-    li      $s1,                0
+    li      $s1,                10
 input_loop: 
     lbu     $t1,                0($t0)
     beq     $t1,                $t2,                endscanf
@@ -267,11 +304,11 @@ input_loop:
     addi    $t1,                $t1,                -0x30
     bltz    $t1,                error                               # if t1 < 0 goto error
     bgt     $t1,                $t3,                error           # if t1 > 10 then goto error
-    mult    $v0,                $s1                                 # $v0 * $s1 = Hi and Lo registers
+    multu   $v0,                $s1                                 # $v0 * $s1 = Hi and Lo registers
     mflo    $v0                                                     # copy Lo to $t2
 
     addu    $v0,                $t1,                $v0             # v0 = v0 + t1
-    bgt     $v1,                $v0,                errorofg        # if $v1 < $v0 then goto errorofg
+    bgtu    $v1,                $v0,                errorofg        # if $v1 < $v0 then goto errorofg
     move    $v1,                $v0                                 # $v1 = $v0
     j       input_loop
 error:      
